58fe150008529b8a60e83b5340a3b4aa7a2bea44
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2024-11-23 03:59:50 -0800

    Changed extension page header, tested overflow and added invisible scroll.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import CourseLine from './components/courseline'
 function App() {
     return (
       <div className="App">
-        <h1>Notified courses</h1>
-        <CourseLine name={"APPLIED DIFFERENTIAL EQUATIONS APPLIED DIFFERENTIAL EQUATIONS"} crn={37631} />
+        <h1>GetTheDamClass: Active Courses</h1>
+        <CourseLine name={"APPLIED DIFFERENTIAL EQUATIONS"} crn={37631} />
         <CourseLine name={"DEEP LEARNING"} crn={37631} />
       </div>
     );
