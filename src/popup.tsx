6590d5f8cf542e992dc49658a362426f1eaeb748
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2024-11-24 00:36:58 -0800

    Fix signup popout window

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,8 +4,14 @@ import SignupForm from './components/signupform';
 import "./App.css";
 
 const root = document.createElement("div")
-root.className = "popup-container"        //This is throwing off the styles for the popup page 
-document.body.appendChild(root)
+root.className = "popup-container"
+
+// Set body for flex
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.height = "100vh";
+document.body.appendChild(root);
 const rootDiv = ReactDOM.createRoot(root);
 
 rootDiv.render(
